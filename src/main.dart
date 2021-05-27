32e4834c812388719d7089ad2cc0169fe5c4ba7a
MysteRys337 <mysteriousfunkyman@gmail.com>
2021-05-26 21:26:00 -0300

    last update

--- src/main.dart
+++ src/main.dart
@@ -9,7 +9,7 @@ void main() {
   ts.add('r2(z), r2(x), r2(y), w2(z)');
   ts.add('r3(y), r3(z), w3(y), r3(x)');
   var sa =
-      'r3(y), r2(z), r1(x), r2(x), r3(z), r2(y), w3(y), w2(z), w1(x), r3(x), r1(z)';
+      'r2(z), r3(y), r1(y), r1(x), w1(x), r2(x), r3(z), r1(z), r2(y), w2(z), w3(y), r3(x)';
 
   print('Sa = ' + sa + '\n\n');
 
